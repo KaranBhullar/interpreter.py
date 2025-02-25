8d4dab3141530a6d5fe3ffe0a0a7a306ef35e8f5
Karan Bhullar <cswailord@gmail.com>
2025-02-24 23:47:39 -0500

    fleshing out the scanner's structure, reading from file, and REPL functions

--- src/scanner.py
+++ src/scanner.py
@@ -1,3 +1,81 @@
+from enum import Enum, auto
+"""
+-> tokens class
+-> lexemes
+-> 
+
+"""
+# I think a better representation is using a dictionary
+class TokenType(Enum):
+    LEFT_PAREN = auto()
+    RIGHT_PAREN = auto()
+    LEFT_BRACE = auto()
+    RIGHT_BRACE = auto()
+    ADD = auto()
+    SUB = auto()
+    MUL = auto()
+    MOD = auto()
+    DIV = auto()
+    EOF = auto()
+    # EOF = auto()
+    # EOF = auto()
+    # EOF = auto()
+    # EOF = auto()
+    # EOF = auto()
+    # EOF = auto()
+    # EOF = auto()
+    # EOF = auto()
+    # EOF = auto()
+    # EOF = auto()
+    # EOF = auto()
+
+# using lookup table for single char tokens and their corresponding values
+single_lookup = {
+    "(": TokenType.LEFT_PAREN,
+    ")": TokenType.RIGHT_PAREN,
+    "{": TokenType.LEFT_BRACE,
+    "}": TokenType.RIGHT_BRACE,
+    "+": TokenType.ADD,
+    "-": TokenType.SUB,
+    "*": TokenType.MUL,
+    "%": TokenType.MOD,
+    "/": TokenType.DIV,
+    # < ... > will fill the rest in a sec
+}
+
+# Probably include object literal in design
+class Token:
+    def __init__(self, token_type: TokenType, lexeme: str, location: list):
+        self.token_type = token_type
+        self.lexeme = lexeme
+        self.location = location # do I need this (maybe)?
+
+
 class Scanner:
-    def __init__(self):
-        self.test = 1
+    def __init__(self, source: str) -> None:
+        self.source = source
+        self.location = [1, 0]
+        self.current = 0
+    
+    # Main Scanning function
+    def scan(self) -> list[Token]:
+        # Should split
+
+        # Placeholder
+        return [Token(single_lookup["+"], "+", [1,1])]
+    
+    # Scanning signle token
+    def scan_token(self) -> None:
+        while (len(self.source) > self.location[1]):
+            print(self.source[self.location[1]])
+            self.location[1] += 1
+        return None
+    
+    # Char @ current position in self.source
+    def next(self):
+        return self.source[self.current]
+    
+    # increments the location of the column in a line
+    def inc(self) -> int:
+        self.location[1] += 1
+        return self.location[1]
--- src/treewalk.py
+++ src/treewalk.py
@@ -1,40 +1,69 @@
 import sys
 import signal
+import os.path
 from types import FrameType
-
+from scanner import Scanner
 """
-not sure if add repl and file in here or in another file
-
 * possibly add running code as a library file *
+
+REPL AND FILE RUNNING SHOULD BE IN SEPARATE FILES
+-> SHOULD HANDLE SIGNALS ACCORDINGLY
 """
+
+# Error handlers | CHANGE THIS TO BE BETTER
+class FileNotFoundError(Exception):
+
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
 
 # Signal handler
 def sigint_handler(signal: int, frame: FrameType | None) -> None:
-    print('signal interrupt')
+    print('Keyboard Interrupt')
+
+def EOF_handler(signal: int, frame: FrameType | None) -> None:
     sys.exit(0)
 
 # Register Signal
 signal.signal(signal.SIGINT, sigint_handler)
+# signal.signal(signal, EOF_handler)
 
 # REPL 
 def repl() -> None:
     stmt = ""
     while (True):
+        stmt = input("> ");
         if stmt == "exit":
             sys.exit(0)
-        stmt = input("> ");
-        print("\n>", stmt, "\n")
+        run(stmt)
 
-# File
-def file() -> None:
+        print(">", stmt) # TEMPORARY
+
+def file(path: str) -> None:
+    if not os.path.isfile(path):
+        print('working')
+        raise FileNotFoundError("File missing brother")
+
+    with open(path, "r") as f:
+        run(f.read())
+        f.close()
+
     return None
+
+def run(src: str) -> None:
+    scanner = Scanner(src)
+    tokens = scanner.scan()
+    for token in tokens:
+        print(token)
 
 def main() -> None:
     if (len(sys.argv) == 1):
         repl()
 
-    elif (len(sys.argv) == 2 and sys.argv[1].endswith(".poo")):
-        print("RUNNING FROM FILE")
+    elif (len(sys.argv) == 2 and sys.argv[1].endswith(".fsh")):
+        print("RUNNING FROM FILE\n")
+        file(sys.argv[1])
 
     else:
         print("Error: Invalid tool call. The tool must be in the format:")
