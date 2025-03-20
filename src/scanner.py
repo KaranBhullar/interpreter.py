7bd3f50834527baa223550e57c8b4025e98f6400
Karan Bhullar <cswailord@gmail.com>
2025-03-20 11:18:05 -0400

    added functionality for recognizing numbers and strings correctly

--- src/scanner.py
+++ src/scanner.py
@@ -1,8 +1,10 @@
 from enum import Enum, auto
+
 """
-    - Add support for literals
-    - Make sure to increment location on \n and inc (I probably won't need logic for col)
+    - Proper checking for integer and floating point values
+    - Should check if next lexeme has decimal and if so the type should be float
 """
+
 # Exception if lexeme is unknown
 class UnknownKeyError(Exception):
     def __init__(self, message) -> None:
@@ -14,6 +16,7 @@ class TokenType(Enum):
     RIGHT_PAREN = auto()
     LEFT_BRACE = auto()
     RIGHT_BRACE = auto()
+
     # Operations
     ADD = auto()
     SUB = auto()
@@ -21,14 +24,18 @@ class TokenType(Enum):
     MOD = auto()
     DIV = auto()
     POW = auto()
+    
     # Literals
-    NUMBER = auto()
+    INTEGER = auto()
+    FLOAT = auto()
     STRING = auto()
     BOOLEAN = auto()
+
     # Misc
     EXMARK = auto()
     DOT = auto()
     COMMA = auto()
+
     # Equality
     GREATER = auto()
     LESS = auto()
@@ -37,10 +44,9 @@ class TokenType(Enum):
     LTEQUAL = auto()
     EQEQUAL = auto()
     NOTEQUAL = auto()
+
     # End of File
     EOF = auto()
-
-
 
 lookup = {
     # Parentheses
@@ -48,6 +54,7 @@ lookup = {
     ")": TokenType.RIGHT_PAREN,
     "{": TokenType.LEFT_BRACE,
     "}": TokenType.RIGHT_BRACE,
+
     # Operations
     "+": TokenType.ADD,
     "-": TokenType.SUB,
@@ -56,15 +63,16 @@ lookup = {
     "/": TokenType.DIV,
     "^": TokenType.POW,
     # Literals
+
     # Misc
     "!": TokenType.EXMARK,
     ".": TokenType.DOT,
     ",": TokenType.COMMA,
+
     # Equality
     ">": TokenType.GREATER, # likely not needed
     "<": TokenType.LESS,
     "=": TokenType.EQUAL,
-    
 }
 
 class Token:
@@ -75,7 +83,9 @@ class Token:
         self.literal = literal
 
     def stringify(self):
-        print(f"{self.lexeme} | {self.token_type} | {self.location}")
+        # print(f"{ self.lexeme: <10 }|  {self.token_type: ^10}| { self.location : >10}")
+
+        print(f"{self.lexeme : <15}{self.token_type : ^15}{self.location[0] : >15}, { self.location[1]}") 
 
 class Scanner:
     def __init__(self, src: str) -> None:
@@ -85,20 +95,60 @@ class Scanner:
         self.curr = 0
         self.tokens: list[Token] = []
     
+    def string(self): # <- ignores last character in string (due to peek function)
+        lexeme = ''
+        self.adv()
+
+        while self.current() != '"':
+            lexeme += self.current()
+            self.pos[1] += 1
+
+            if not self.peek() and self.current() != '"': 
+                raise UnknownKeyError("String not closed")
+
+            self.adv()
+
+        self.add_token(TokenType.STRING, lexeme, self.pos, str)
+
+    def number(self):
+        lexeme = ''
+        token_type = TokenType.INTEGER
+
+        while self.current().isdigit():
+            lexeme += self.current()
+
+            if self.next('.'):
+                if '.' in lexeme: raise UnknownKeyError("Invalid float representation")
+                token_type = TokenType.FLOAT
+                lexeme += self.current()
+
+            if not self.peek() or not self.peek().isdecimal():
+                print("done from number")
+                break
+
+            self.adv()
+
+        self.add_token(token_type, lexeme, self.pos, int)
+
+
     def scan(self) -> list:
-        print(self.src)
+        print(self.src) # Temporary
+
         while (not self.completed()):
             self.match()
+
         self.add_token(TokenType.EOF, '\0', self.pos, None)
         return self.tokens
 
 
     def completed(self):
         if (self.curr >= len(self.src)): return True
+
         return False
     
     def match(self) -> None:
-        self.start = self.curr
+        self.start = self.curr # <-- May need changing because lexemes are shown at their ending position, not starting position
+
         match self.current():
             case '!':
                 self.add_token(TokenType.NOTEQUAL, '!=', self.pos, None) if self.next('=') else self.add_token(TokenType.EXMARK, '!', self.pos, None)
@@ -110,19 +160,25 @@ class Scanner:
                 self.add_token(TokenType.EQEQUAL, '==', self.pos, None) if self.next('=') else self.add_token(TokenType.EQUAL, '=', self.pos, None)
             case '/':
                 pass
+            case '"':
+                self.string()
+            case _ if self.current().isdecimal():
+                self.number()
             case ' ' | '\t' | '\r': # types of whitespace
-                pass # should be ignored for the most part
+                pass # Whitespaces should be ignored
             case '\n':
                 self.pos[0] += 1
                 self.pos[1] = 0
             case _:
                 if self.current() not in lookup: raise UnknownKeyError("An unknown character has occurred")
                 self.add_token(lookup[self.current()], self.current(), self.pos) # base case for any single-char lexeme
+
         self.pos[1] += 1
         self.adv()
 
     def add_token(self, token_type: TokenType, lexeme: str, pos: list, literal=None) -> None:
-        self.tokens.append(Token(token_type, lexeme, pos.copy(), literal)) # error before was that I was passing by reference, not by value
+        self.tokens.append(Token(token_type, lexeme, pos.copy(), literal)) 
+        return
 
     def current(self) -> str:
         return self.src[self.curr]
@@ -133,15 +189,17 @@ class Scanner:
 
     def peek(self) -> str | None:
         if self.completed() or self.curr == len(self.src) - 1: return None
+
         return self.src[self.curr + 1]
-        # return self.src[self.curr + 1] if (not self.completed()) else None
 
     def next(self, char: str):
         next = True if (self.peek() == char) else False
-        if next: self.adv();self.pos[1] +=1
+        if next: self.adv();self.pos[1] +=1 # adv and increment the location by 1
+
         return next
 
     # Test function
-    def to_str(self):
+    def to_str(self) -> None:
         for token in self.tokens:
             token.stringify()
+        return
